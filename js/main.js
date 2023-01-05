a9bcb93384978fe776ea2ea07acb8c75466ad702
Victoria Johnson <johnsonvickie884@gmail.com>
2023-01-05 13:42:38 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+class Books {
+    constructor (title, author, pages){
+        this.title = title;
+        this.author = author;
+        this. pages = pages;
+    }
+};
+
+let myBook = new Books ('What do you know about classes?', 'Victoria Johnson', '45');
