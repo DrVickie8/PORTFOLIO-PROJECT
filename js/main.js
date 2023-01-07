dbd911d5cef7c3dd3e6c984d858f4e4af92814b2
Victoria Johnson <johnsonvickie884@gmail.com>
2023-01-07 16:08:50 +0100

    Scripted JS to toggle the menu for mobile screen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
-class Books {
-    constructor (title, author, pages){
-        this.title = title;
-        this.author = author;
-        this. pages = pages;
-    }
-};
+// class Books {
+//     constructor (title, author, pages){
+//         this.title = title;
+//         this.author = author;
+//         this. pages = pages;
+//     }
+// };
 
-let myBook = new Books ('What do you know about classes?', 'Victoria Johnson', '45');
+// if (pages === 0){
+//     this.pagesRead = 'Not yet read'
+// }else{
+//     this.pagesRead = 'Has been read'
+// };
+
+// let firstUser = new Books ('What do you know about classes?', 'Victoria Johnson', 45);
+// let secondUser = new Books ('What do you know about classes?', 'Victoria Johnson', 0);
+// let thirdUser = new Books ('What do you know about classes?', 'Victoria Johnson', 20);
+
+
+// MENU SHOW Y HIDDEN
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+// MENU SHOW
+// Validate if constant exists
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// MENU HIDDEN
+// Validate if constant exists
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
