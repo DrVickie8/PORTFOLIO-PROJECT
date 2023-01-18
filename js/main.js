ad00c5c22db371fe2d4010f2fae21693819cf5fd
Victoria Johnson <johnsonvickie884@gmail.com>
2023-01-18 05:52:00 +0100

    Adjusted the services section

--- js/main.js
+++ js/main.js
@@ -78,27 +78,27 @@ skillsHeader.forEach((el) =>{
 
 
 /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services_modal'),
-      modalBtns = document.querySelectorAll('.services_button'),
-      modalCloses = document.querySelectorAll('.services_modal-close')
+// const modalViews = document.querySelectorAll('.services_modal'),
+//       modalBtns = document.querySelectorAll('.services_button'),
+//       modalCloses = document.querySelectorAll('.services_modal-close')
 
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}
+// let modal = function(modalClick){
+//     modalViews[modalClick].classList.add('active-modal')
+// }
 
-modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener('click', () => {
-        modal(i)
-    })
-})
+// modalBtns.forEach((modalBtn, i) => {
+//     modalBtn.addEventListener('click', () => {
+//         modal(i)
+//     })
+// })
 
-modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click', () =>{
-        modalViews.forEach((modalView) =>{
-            modalView.classList.remove('active-modal')
-        })
-    })
-})
+// modalCloses.forEach((modalClose) => {
+//     modalClose.addEventListener('click', () =>{
+//         modalViews.forEach((modalView) =>{
+//             modalView.classList.remove('active-modal')
+//         })
+//     })
+// })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
     let swiper = new Swiper('.portfolio_container', {
